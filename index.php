98acc1041b6291bac4745378071e39161df7e9b1
Rubén Peinado <rpeinado@asystem.es>
2018-10-03 16:58:10 +0200

    phpmailer

--- index.php
+++ index.php
@@ -1,34 +1,61 @@
 <?
-
-//die("pipi");
 error_reporting(E_ALL);
 ini_set('display_errors',	1);
-//$path_log= __DIR__ . '/' . 'log.log';
-//ini_set('error_log', $path_log);
-//pinta($path_log);
+require("phpmailer/class.phpmailer.php");
 
-//$po= 4/0; echo "(($po))"; // dsdfsd
+define("email_host", "mail.umates.com");
+define("email_defauthusername", "uniflip@uniflip.com");
+define("email_defauthpassword", "UniMail200");
+define("email_smtpauth", false);
+define("email_deffrom", "uniflip@uniflip.com");
+define("email_deffromname", "Mailer uniflip.com");
+define("email_defreplyto", "uniflip@uniflip.com");
+define("email_defreplytoname", "Support uniflip.com");
 
+$from= 'rpeinado@asystem.es';
 $to= 'rpeinado@asystem.es';
 $subject= 'the subject';
 $message= 'the test message';
-$headers= 'From: uniflip@uniflip.com' . "\r\n" .
-		  'Reply-To: uniflip@uniflip.com' . "\r\n" .
+$headers= 'From: ' . $from . "\r\n" .
+		  'Reply-To: ' . $from . "\r\n" .
 		  'X-Mailer: PHP/' . phpversion();
 
-$result= mail($to, $subject, $message, $headers);
+	$mail = new PHPMailer();
+  	$mail->CharSet  = "UTF-8";
+	$mail->Host     = email_host;
+	$mail->IsSMTP();
+	$mail->SMTPAuth = true;
+	$mail->Username = email_defauthusername;
+	$mail->Password = email_defauthpassword;
+	$mail->From     = $from;
+	$mail->FromName = $from;
+	$mail->Subject  = $subject;
+	$mail->Body     = $message;
+	$mail->AltBody  = $message;
+	$mail->AddAddress($to, $to);
+	$mail->AddReplyTo($from, $from);
+	$mail->AddReplyTo($from, $from);
 
-pinta($result);
+	$result= $mail->Send();
 
-if (!$result)
-{
-	print_r(error_get_last());
-	die("not sent!!");
-}
-else
-{
-	die("success!!");
-}
+	$mail->ClearAddresses();
+
+
+	//$result= mail($to, $subject, $message, $headers);
+
+	pinta($result);
+
+	if (!$result)
+	{
+		print_r(error_get_last());
+		die("not sent!!");
+	}
+	else
+	{
+		die("success!!");
+	}
+
+	unset($mail);
 
 
 function pinta($data, $end= false)
